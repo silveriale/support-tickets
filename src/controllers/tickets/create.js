ddd5ad5da9436a1a3263af335352d017aa4e2949
silveriale <leticia.silveria.costa@gmail.com>
2025-07-15 11:26:13 -0300

    Criando o objeto de ticket
    - objeto de ticket, utilizando a rota "barra tickets" com o método "post"
    - identificador único para cada ticket, utilizando o Crypto do Node.js.
    -definição de um status padrão "open" e a inclusão de datas de criação e atualização

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto";
+
 export function create({ request, response }) {
-  return response.end("Criado com sucesso!");
+  // Função para criar um ticket
+  const { equipment, description, user_name } = request.body;
+
+  const ticket = {
+    id: randomUUID(), // Gera um ID único para o ticket
+    equipment,
+    description,
+    user_name,
+    status: "open", // Define o status inicial do ticket como "open"
+    created_at: new Date(), // Registra a data de criação do ticket
+    updated_at: new Date(), // Registra a data de atualização do ticket
+  };
+  return response.end(JSON.stringify(ticket)); // Retorna o ticket criado como resposta
 }
