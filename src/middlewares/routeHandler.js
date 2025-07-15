4281cf6ca7e79584bf68c853f393ece52e495193
silveriale <leticia.silveria.costa@gmail.com>
2025-07-15 10:55:54 -0300

    middleware para lidar com rotas em uma aplicação.

---
+++ src/middlewares/routeHandler.js
@@ -0,0 +1,15 @@
+import { routes } from "../routes/index.js";
+
+export function routeHandler(request, response) {
+  // Define a rota para obter todos os tickets
+  const route = routes.find((route) => {
+    return route.method === request.method && route.path === request.url;
+  });
+
+  // Se a rota for encontrada, chama o controlador
+  if (route) {
+    return route.controller(request, response);
+  }
+  // Se a rota não for encontrada, retorna um erro 404
+  return response.writeHead(404).end();
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import http from "node:http";
 
 import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 // Cria um servidor HTTP que escuta na porta 3333 e usa o middleware jsonHandler para processar requisições JSON
 async function listener(request, response) {
   await jsonHandler(request, response);
-  console.log(request.body);
+  routeHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);
