9d131e2baee8e1c1e504174ac729f08b7502e4f9
silveriale <leticia.silveria.costa@gmail.com>
2025-07-25 17:23:49 -0300

    Rota para atualizar status do ticket
    - nova requisição para encerrar um ticket, utilizando o método PATCH para modificar o status específico do ticket para "fechado"

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,12 +1,14 @@
 export function update({ request, response, database }) {
-  const { id } = request.params;
-  const { equipment, description } = request.body;
+  // Controlador para atualizar um ticket
+  const { id } = request.params; // Extrai o ID do ticket dos parâmetros da solicitação
+  const { equipment, description } = request.body; // Extrai os dados do equipamento e descrição do corpo da solicitação
 
   database.update("tickets", id, {
+    // Atualiza o ticket com os novos dados
     equipment,
     description,
     updated_at: new Date(),
   });
 
-  return response.end();
+  return response.end(); // Retorna uma resposta vazia após a atualização bem-sucedida
 }
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,22 +1,28 @@
 import { create } from "../controllers/tickets/create.js";
 import { index } from "../controllers/tickets/index.js";
 import { update } from "../controllers/tickets/update.js";
+import { updateStatus } from "../controllers/tickets/updateStatus.js";
 
 export const tickets = [
   // Define a rota para obter todos os tickets
   {
     method: "POST",
     path: "/tickets",
-    controller: create,
+    controller: create, // Controlador para criar um novo ticket
   },
   {
     method: "GET",
     path: "/tickets",
-    controller: index,
+    controller: index, // Controlador para obter todos os tickets
   },
   {
     method: "PUT",
     path: "/tickets/:id",
-    controller: update,
+    controller: update, // Controlador para atualizar um ticket
+  },
+  {
+    method: "PATCH",
+    path: "/tickets/:id/close",
+    controller: updateStatus, // Controlador para atualizar o status de um ticket
   },
 ];
