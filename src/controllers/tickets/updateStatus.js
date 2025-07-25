2835b3d6c08d00cae554ab485e16a8794b133dc4
silveriale <leticia.silveria.costa@gmail.com>
2025-07-25 17:33:08 -0300

    Atualizando o status do ticket
    - atualiza o status de um ticket no banco de dados sem a necessidade de criar um novo método
    - é possível alterar o status para "fechado" apenas passando o ID do ticket e o novo status desejado
    - após a atualização, é possível verificar a mudança no status dos tickets abertos e fechados.

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,3 +1,9 @@
 export function updateStatus({ request, response, database }) {
-  return response.end("ok");
+  const { id } = request.params; // Extrai o ID do ticket dos parâmetros da solicitação
+
+  database.update("tickets", id, {
+    status: "closed",
+  }); // Define o status do ticket como fechado
+
+  return response.end();
 }
