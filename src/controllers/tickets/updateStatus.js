cba3ab986fe6c2197c186d8d1dd8eb664597bd39
silveriale <leticia.silveria.costa@gmail.com>
2025-07-28 17:19:21 -0300

    Adicionando a solução no ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,9 +1,12 @@
 export function updateStatus({ request, response, database }) {
   const { id } = request.params; // Extrai o ID do ticket dos parâmetros da solicitação
 
+  const { solution } = request.body; // Extrai a solução do corpo da solicitação
+
   database.update("tickets", id, {
     status: "closed",
-  }); // Define o status do ticket como fechado
+    solution,
+  }); // Define o status do ticket como fechado e adiciona a solução
 
   return response.end();
 }
